8045a2104bbd24cf38dfe30e1b368f195d752f0c
claudiarelat <clauprelat@gmail.com>
2025-07-01 21:57:14 +0200

    he quitado imágenes vite y react

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
+import './styles/App.scss'
 import App from './App.jsx'
 import { HashRouter } from 'react-router-dom'
 

02b26f036917de7550a02a3a3c17e65de9892dab
Paula <paulasanjosemartin@gmail.com>
2025-07-01 22:08:25 +0200

    nombre y h1

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './styles/App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <h1>Brag Space</h1>
     </>
   )
 }

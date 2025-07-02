02b354210fb37519f2068b7adb413eb727e57523
claudiarelat <clauprelat@gmail.com>
2025-07-02 20:26:15 +0200

    añadido form en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './styles/App.scss'
+import Form from './components/Form.jsx'
 
 function App() {
 
   return (
     <>
       <h1>Brag Space</h1>
+      <Form />
     </>
   )
 }

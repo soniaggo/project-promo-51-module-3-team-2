3543c7c5c339b880763fff0d989de70839568c99
soniaggo <sonigit config --global user.email lamary@gmail.com>
2025-07-08 20:23:12 +0200

    add form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -50,22 +50,22 @@ function Form({ pformData, psetFormData, projectImage, setProjectImage, authorIm
       <h2 className="title">Crea tu claqueta</h2>
       <fieldset className="addForm__group" />
       <p>Cuéntanos sobre tu producción favorita</p>
-      <input className="addForm__input" type="text" name="name" id="name" placeholder="Título de la pieza audiovisual" value={pformData.name} onChange={handleChange} />
-      <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Frase estrella" value={pformData.slogan} onChange={handleChange} />
+      <input className="addForm__input" type="text" name="name" id="name" placeholder="Título de la pieza audiovisual" value={pformData.name} onChange={handleChange}  required/>
+      <input className="addForm__input" type="text" name="slogan" id="slogan" placeholder="Frase estrella" value={pformData.slogan} onChange={handleChange} required/>
       <div className="addForm__2col" />
-      <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Añade un enlace a la página de reseñas" value={pformData.repo} onChange={handleChange} />
-      <input className="addForm__input" type="url" name="web" id="web" placeholder="Añade la plataforma donde se pueda reproducir" value={pformData.web} onChange={handleChange} />
-      <input className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Género" value={pformData.technologies} onChange={handleChange} />
-      <textarea className="addForm__input" name="desc" id="desc" placeholder="Sinopsis" rows="5" value={pformData.desc} onChange={handleChange}></textarea>
+      <input className="addForm__input" type="url" name="repo" id="repo" placeholder="Añade un enlace a la página de reseñas" value={pformData.repo} onChange={handleChange} required/>
+      <input className="addForm__input" type="url" name="web" id="web" placeholder="Añade la plataforma donde se pueda reproducir" value={pformData.web} onChange={handleChange} required/>
+      <input className="addForm__input" type="text" name="technologies" id="technologies" placeholder="Género" value={pformData.technologies} onChange={handleChange} required />
+      <textarea className="addForm__input" name="desc" id="desc" placeholder="Sinopsis" rows="5" value={pformData.desc} onChange={handleChange} required></textarea>
       <fieldset className="addForm__group"></fieldset>
       <p>Cuéntanos sobre tu personaje favorito</p>
-      <input className="addForm__input" type="text" name="autor" id="autor" placeholder="Nombre del personaje" value={pformData.autor} onChange={handleChange} />
-      <input className="addForm__input" type="text" name="job" id="job" placeholder="Rol del personaje" value={pformData.job} onChange={handleChange} />
+      <input className="addForm__input" type="text" name="autor" id="autor" placeholder="Nombre del personaje" value={pformData.autor} onChange={handleChange} required/>
+      <input className="addForm__input" type="text" name="job" id="job" placeholder="Rol del personaje" value={pformData.job} onChange={handleChange} required/>
       <fieldset className="addForm__group--upload"></fieldset>
       <label htmlFor="image" className="button">Subir portada de la producción</label>
-      <input className="addForm__hidden" type="file" id="image" name="image" onChange={handleProjectImageUpload} />
+      <input className="addForm__hidden" type="file" id="image" name="image" onChange={handleProjectImageUpload} required />
       <label htmlFor="photo" className="button">Subir foto del personaje</label>
-      <input className="addForm__hidden" type="file" name="photo" id="photo" onChange={handleAuthorImageUpload} />
+      <input className="addForm__hidden" type="file" name="photo" id="photo" onChange={handleAuthorImageUpload} required />
       <button className="button__large" type="submit">¡ACCIÓN!</button>
     </form>
   );
